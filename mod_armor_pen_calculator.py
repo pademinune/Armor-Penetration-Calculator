711eee433ac78ae6693f8e2c40fc9d292d9fe8c1
pademinune <justinnitoi@gmail.com>
2026-03-30 23:09:53 -0400

    got rid of useless parameters

--- mod_armor_pen_calculator.py
+++ mod_armor_pen_calculator.py
@@ -19,13 +19,14 @@ log("Mod is loading")
 
 # Create a text component
 label = GUI.Text("")
-label.font = "default_large.font" # Or "default_medium.font"
-label.filterType = "LINEAR"
-label.horizontalAnchor = "CENTER"
-label.verticalAnchor = "CENTER"
-label.horizontalPositionMode = "SCREEN_RELATIVE"
-label.verticalPositionMode = "SCREEN_RELATIVE"
-label.size = (0.5, 0.04)
+# label.font = "default_medium.font" # Or "default_medium.font"
+# label.filterType = "LINEAR"
+# label.horizontalAnchor = "CENTER"
+# label.verticalAnchor = "CENTER"
+# label.horizontalPositionMode = "SCREEN_RELATIVE"
+# label.verticalPositionMode = "SCREEN_RELATIVE"
+
+
 label.position = (0, -0.1, 0.5) # Center of screen, slightly below crosshair
 label.visible = False
 GUI.addRoot(label)
