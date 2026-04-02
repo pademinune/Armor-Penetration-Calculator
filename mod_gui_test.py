9ce27597136aea6e7a9e3e691cb9d9b744e2ce75
pademinune <justinnitoi@gmail.com>
2026-04-02 15:08:18 -0400

    cleaned up files

--- mod_gui_test.py
+++ mod_gui_test.py
@@ -11,12 +11,6 @@ log("Mod is loading")
 
 # Create a text component
 label = GUI.Text("")
-label.font = "system_medium" # Or "default_medium.font"
-label.filterType = "LINEAR"
-label.horizontalAnchor = "CENTER"
-label.verticalAnchor = "CENTER"
-label.horizontalPositionMode = "SCREEN_RELATIVE"
-label.verticalPositionMode = "SCREEN_RELATIVE"
 label.position = (0, -0.1, 0.5) # Center of screen, slightly below crosshair
 label.visible = False
 GUI.addRoot(label)
