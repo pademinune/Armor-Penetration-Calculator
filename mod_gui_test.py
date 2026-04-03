bece9a343773c72f83a1a2a0e4b35fe80727c606
Justin <justinnitoi@gmail.com>
2026-04-03 14:39:03 -0400

    created new gui test

--- mod_gui_test.py
+++ mod_gui_test.py
@@ -1,23 +1,55 @@
 
-import math
-import GUI
-from debug_utils import LOG_WARNING
-
+from gambiter import g_guiFlash
+from gambiter.flash import COMPONENT_TYPE, COMPONENT_ALIGN
 
 def log(message):
-    LOG_WARNING("pademinune: " + str(message))
+    print("pademinune's GuiTest mod: " + str(message))
 
-log("Mod is loading")
+log("GUIFlash Test Mod is loading")
 
-# Create a text component
-label = GUI.Text("")
-label.position = (0, -0.1, 0.5) # Center of screen, slightly below crosshair
-label.visible = False
-GUI.addRoot(label)
 
-label.text = "THIS IS THE LABEL"
-label.colour = (255, 0, 0, 255)
-label.visible = True
+armor_value_properties = {
+    'text': "<font size='25' color='#6BF40D' face='$FieldFont'>150mm</font>",
+    'multiline': True,
+    'glowfilter': {
+        'color': 0x000000,   # Black
+        'alpha': 1,          # Solid
+        'blurX': 3,          # Glow width
+        'blurY': 3,          # Glow height
+        'strength': 10,      # Higher = sharper outline
+        'quality': 2
+    },
+    'alignX': COMPONENT_ALIGN.CENTER,
+    'alignY': COMPONENT_ALIGN.CENTER,
+    'x': 0,
+    'y': 30,
+}
 
-log("Mod has finished loading")
+probability_properties = {
+    'text': "<font size='25' color='#6BF40D' face='$FieldFont'>100%</font>",
+    'multiline': True,
+    'glowfilter': {
+        'color': 0x000000,   # Black
+        'alpha': 1,          # Solid
+        'blurX': 3,          # Glow width
+        'blurY': 3,          # Glow height
+        'strength': 10,      # Higher = sharper outline
+        'quality': 2
+    },
+    'alignX': COMPONENT_ALIGN.CENTER,
+    'alignY': COMPONENT_ALIGN.CENTER,
+    'x': 0,
+    'y': 60,
+}
+
+
+alias = "pademinune_ArmorPenLabel"
+component_type = COMPONENT_TYPE.LABEL
+
+# create the armor value label
+g_guiFlash.createComponent(alias, component_type, armor_value_properties)
+# create the probability label
+g_guiFlash.createComponent("pademinune_probabilityLabel", component_type, probability_properties)
+
+log("GUIFlash Test Mod has finished loading")
 
