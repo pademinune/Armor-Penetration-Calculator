7008035d2f53c495b6b828d5130e08f24802a24f
Justin <justinnitoi@gmail.com>
2026-04-27 17:18:02 -0400

    builds now add the crosshair image

--- build_nightly.ps1
+++ build_nightly.ps1
@@ -1,5 +1,6 @@
 
 $DEST = "C:\Program Files (x86)\World_of_Tanks_NA\res_mods\2.2.1.1\scripts\client\gui\mods"
+$IMAGE_DEST = "C:\Program Files (x86)\World_of_Tanks_NA\res_mods\2.2.1.1\gui\pademinune"
 
 python27 -m py_compile mod_armor_pen_calculator.py
 Move-Item -Force mod_armor_pen_calculator.pyc bin/
@@ -18,6 +19,10 @@ Copy-Item bin/mod_armor_pen_calculator.pyc $DEST
 Copy-Item bin/pade_constants.pyc $DEST
 Copy-Item bin/pade_gui.pyc $DEST
 Copy-Item bin/pade_config.pyc $DEST
+
+Copy-Item .\images\crosshair-16-green.png $IMAGE_DEST
+Copy-Item .\images\crosshair-16-orange.png $IMAGE_DEST
+Copy-Item .\images\crosshair-16-red.png $IMAGE_DEST
 
 
 Write-Output "Compiled and copied nightly branch mod files to '$DEST'"
--- uninstall_nightly.ps1
+++ uninstall_nightly.ps1
@@ -1,9 +1,15 @@
 
 $DEST = "C:\Program Files (x86)\World_of_Tanks_NA\res_mods\2.2.1.1\scripts\client\gui\mods"
+$IMAGE_DEST = "C:\Program Files (x86)\World_of_Tanks_NA\res_mods\2.2.1.1\gui\pademinune"
 
 Remove-Item $DEST/mod_armor_pen_calculator.pyc -Force
 Remove-Item $DEST/pade_constants.pyc -Force
 Remove-Item $DEST/pade_gui.pyc -Force
 Remove-Item $DEST/pade_config.pyc -Force
+
+
+Remove-Item $IMAGE_DEST\crosshair-16-green.png -Force
+Remove-Item $IMAGE_DEST\crosshair-16-orange.png -Force
+Remove-Item $IMAGE_DEST\crosshair-16-red.png -Force
 
 Write-Output "Removed all 4 nightly branch files from '$DEST'"
